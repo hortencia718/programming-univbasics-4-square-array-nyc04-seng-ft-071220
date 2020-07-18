f2443c822809b9ef5e4b4ef32d2bab241d8e5bdb
hortencia c <hortenciaecisneros@gmail.com>
2020-07-18 23:39:05 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,10 @@
 
 def square_array(array)
+  # while array.length is greater (3) than counter 
   counter = 0 
   while array.length > counter do 
     p "hi"
-      counter =+ 1 
+      counter += 1 
   end  
 # num * num
 end
