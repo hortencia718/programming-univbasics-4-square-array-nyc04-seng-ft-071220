216fc0c50d88babf77df666686579a890cbd1e82
hortencia c <hortenciaecisneros@gmail.com>
2020-07-19 00:03:20 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,7 +4,7 @@ def square_array(array)
   counter = 0 
   new_numbers =[]
   while array.length > counter do 
-    # num = array[counter]
+     num = array[counter]
     new_numbers.push(num*num)
       counter += 1 
   end 
